b8784826d46a46d5c92df4a997ba0b887e8fcc87
m2构造机 <zhanggaoqi@aobi.com>
2026-07-07 16:50:09 +0800

    Release v2.0.2
    - 更新所有 XCFrameworks 到 2.0.2
    - 更新资源文件
    - 更新 checksums
    - 自动生成 Info.plist
    - 修复资源路径配置

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "BTCocoaCategory",
             url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/BTCocoaCategory/2.0.2/BTCocoaCategory.xcframework.zip",
-            checksum: "f2837f01ab1026437b8218f1b90046a35742cebe4b51c5e1a6c2022912d7fcc9"
+            checksum: "0c4f8cea3bdda6a1cb17c9313bbfcc911db4bca2e56da1e14e64c31a1e7c83e0"
         )
     ]
 )
