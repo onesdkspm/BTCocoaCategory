f68c1086058e8d665b830d344bf440dc3b06e9c1
m2构造机 <zhanggaoqi@aobi.com>
2026-07-08 17:18:48 +0800

    [Branch Release] v2.0.3-dev-1481311 - develop
    - 更新所有 XCFrameworks 到 2.0.3-dev-1481311
    - 更新资源文件
    - 更新 checksums
    - 分支测试版本（无 tag）

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
         // ========== Binary Frameworks ==========
         .binaryTarget(
             name: "BTCocoaCategory",
-            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/BTCocoaCategory/2.0.2-dev-1480282/BTCocoaCategory.xcframework.zip",
-            checksum: "4388f9226453819341527cc3a7108288bac81b4b015de193e510450a16f40e23"
+            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/BTCocoaCategory/2.0.3-dev-1481311/BTCocoaCategory.xcframework.zip",
+            checksum: "3744271603eb4603b0a4de3799d3a36ed10df1bffd4cc06c3d529ae0c041c0c2"
         )
     ]
 )
